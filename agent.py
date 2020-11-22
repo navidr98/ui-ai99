bc2eddeeaf86ef75c724165e567bf95f5cbdac63
captainhb <hossein1377075@gmail.com>
2020-11-22 20:12:22 +0330

    added agent functions

--- agent.py
+++ agent.py
@@ -2,7 +2,37 @@ import random
 from base import BaseAgent, TurnData, Action
 
 
+class State:
+    def __init__(self):
+        self.map = None
+        self.carrying = False
+
+
+class Node:
+    def __init__(self, parent_node: State = None, parent_action: Action = None):
+        self.parent_node = parent_node
+        self.parent_action = parent_action
+        self.state = None
+        self.cost = 0
+
+    def deduce_state(self):
+        """deduces the state that caused by parent_action"""
+        pass
+
+    def calculate_cost(self):
+        """adds the cost of parent_action to parent_node.cost"""
+        pass
+
+
 class Agent(BaseAgent):
+
+    def __init__(self):
+        BaseAgent.__init__(self)
+        print(f"MY NAME: {self.name}")
+        print(f"PLAYER COUNT: {self.agent_count}")
+        print(f"GRID SIZE: {self.grid_size}")
+        print(f"MAX TURNS: {self.max_turns}")
+        print(f"DECISION TIME LIMIT: {self.decision_time_limit}")
 
     def do_turn(self, turn_data: TurnData) -> Action:
         print(f"TURN {self.max_turns - turn_data.turns_left}/{self.max_turns}")
@@ -23,6 +53,19 @@ class Agent(BaseAgent):
         if action_name == "R":
             return Action.RIGHT
         return random.choice(list(Action))
+
+    def breadth_first_search(self):
+        pass
+
+    def pop(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def goal_test(self, child_state: State):
+        pass
+
 
 if __name__ == '__main__':
     winner = Agent().play()
