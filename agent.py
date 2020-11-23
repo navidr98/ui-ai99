73e0b572631be10e5393a82e7c650305dacb3d7d
captainhb <hossein1377075@gmail.com>
2020-11-23 15:16:38 +0330

    completed State and Node classes and modified breadth_first_search()

--- agent.py
+++ agent.py
@@ -6,33 +6,92 @@ class State:
     def __init__(self):
         self.map_data = []
         self.carrying = None
-        self.turns_left = None
         self.position = ()
         self.collected_list = []
+    
+    def __eq__(self, other_state):
+        if other_state.map_data != self.map_data:
+            return False
+        elif other_state.carrying != self.carrying:
+            return False
+        elif other_state.position != self.position:
+            return False
+        elif other_state.collected_list != self.collected_list:
+            return False
+        
+        return True
 
 
 class Node:
-    def __init__(self, parent_node: State = None, parent_action: Action = None):
-        self.parent_node = parent_node
-        self.parent_action = parent_action
-        self.state = None
+    def __init__(self, parent_node = None, action: Action = None, init_state = None):
+        self.parent = parent_node
+        self.parent_action = action
         self.cost = 0
 
+        if init_state == None:
+            self.state = State()
+            self.deduce_state()
+        else:
+            self.state = init_state
+
     def deduce_state(self):
+        self.cost = self.parent.cost + 1
         """deduces the state that caused by parent_action"""
-        pass
 
-    def calculate_cost(self):
-        """adds the cost of parent_action to parent_node.cost"""
-        pass
+        if self.parent_action == Action.UP:  # set position of next state
+            self.state.position[0] = self.parent.state.position[0] - 1
+            self.state.position[1] = self.parent.state.position[1]
+        elif self.parent_action == Action.DOWN:
+            self.state.position[0] = self.parent.state.position[0] + 1
+            self.state.position[1] = self.parent.state.position[1]
+        elif self.parent_action == Action.RIGHT:
+            self.state.position[1] = self.parent.state.position[1] + 1
+            self.state.position[0] = self.parent.state.position[0]
+        elif self.parent_action == Action.LEFT:
+            self.state.position[1] = self.parent.state.position[1] - 1
+            self.state.position[0] = self.parent.state.position[0]
+        
+        symbol_in_map = self.parent.state.map_data[self.state.position[0]][self.state.position[1]]
+        # set map of next state
+        if symbol_in_map == '.':  # empty 
+            self.state.map_data = self.parent.state.map_data
+            self.state.carrying = self.parent.state.carrying
+            self.state.collected_list = self.parent.state.collected_list
+        elif symbol_in_map == 'a':  # agent's base
+            self.state.map_data = self.parent.state.map_data
+            if self.parent.state.carrying != None:  # agent is carrying a diamond
+                self.state.collected_list.append(self.parent.state.carrying)
+                self.state.carrying = None
+            else:  # agent is not carrying a diamond
+                self.state.collected_list = self.parent.state.collected_list
+                self.state.carrying = None
+        elif int(symbol_in_map) >= 0:  # some diamond
+            if self.parent.state.carrying != None:  # agent is carrying a diamond
+                self.state.map_data = self.parent.state.map_data
+                self.state.carrying = self.parent.state.carrying
+                self.state.collected_list = self.parent.state.collected_list
+            else:  # agent is not carrying diamond
+                self.state.map_data = self.parent.state.map_data
+                self.state.carrying = int(symbol_in_map)
+                self.state.map_data[self.state.position[0]][self.state.position[1]] = '.'
+                self.state.collected_list = self.parent.state.collected_list
 
-
+    def __eq__(self, other_node):
+        if other_node.parent != self.parent:
+            return False
+        elif other_node.parent_action != self.parent:
+            return False
+        elif other_node.state != self.state:
+            return False
+        elif other_node.cost != self.cost:
+            return False
+        return True
 
 class Agent(BaseAgent):
 
     def __init__(self):
         BaseAgent.__init__(self)
-        self.start_state = None
+        self.start_node = None
         self.diamond_state = None
         self.is_first_turn = True
         self.solution_list = []
@@ -57,26 +116,58 @@ class Agent(BaseAgent):
         # returning agent action by calling breadth_first_search(turnData) 
 
         # temporary
-        return Action.RIGHT
+        return random.choice(action)
 
 
     def breadth_first_search(self, turn_data: TurnData) -> Action:
         """performs breadth first search on problem"""
         current_state = self.transform_turnData_to_state(turn_data)
-        x = self.actions(current_state)
-        print(x)
-        return None ##################################################3
+       
+        # possible_actions = self.actions(current_state) ##########################
+        # for action in possible_actions:##############################3
+        #     child = self.child_node(parent=self.start_node, action=action)#####################
+        #     with open("test-child-node", "a") as f:
+        #         f.write("\n\n###############################")
+        #         f.write("\nmap: " + str(child.state.map_data))
+        #         f.write("\ncarrying: " +  str(child.state.carrying))
+        #         f.write("\ncollected_list: " +  str(child.state.collected_list))
+        #         f.write("\npostion: " + str(child.state.position))
+        #         f.write("\ncost: " + str(child.cost))
+        #         f.write("################################")
+        # return possible_actions ##################################################3
         frontier = []
-        explored_set = {}
+        explored_set = []
 
         if self.is_first_turn:
-            self.start_state = current_state
+            self.start_node = Node(init_state=current_state)
+            frontier.append(self.start_node)
             self.is_first_turn = False
-        elif current_state == self.start_state:
+
+            while True:
+                if not frontier:  # list is empty
+                    return -1
+                node = frontier.pop(0)  # chooses the shallowest node in frontier
+                explored_set.append(node.state)
+
+                for action in self.actions(node.state):
+                    child = self.child_node(node, action)
+                    if (child.state not in explored_set) and (child not in frontier):
+                        if self.goal_test(child.state):
+                            # save solution to solution_list
+                            # save first action of the solution list
+                            # update solution list
+                            # return first action
+                            return self.solution(child)##################################
+
+                        frontier.append(child)
+                        
+
+                        
+
+                    
             # first creating relative goal state
             # do searching diamond state
             # creating solution_list actions
-            pass
         elif current_state == self.diamond_state:
             # first creating relative final goal state
             # do searching for final goal
@@ -103,14 +194,8 @@ class Agent(BaseAgent):
 
         return state
 
-    def pop(self):
-        pass
-
-    def insert(self):
-        pass
-
-    def goal_test(self, child_state: State, goal_state: State):
-        pass
+    def goal_test(self, child_state: State):
+        return None
 
     def actions(self, state: State) -> list:
         """returns list of possible actions"""
@@ -142,7 +227,16 @@ class Agent(BaseAgent):
 
         return possible_actions
     
-    def child_node(self, state: State, action: Action, parent_cost: int) -> Node:
+    def child_node(self, parent : Node, action: Action) -> Node:
+        """creates a child node with provided parameter"""
+        child_node = Node(parent, action)
+        return child_node
+
+    def solution(self, child: Node):
+        return None
+        
+
+
         
 
 
